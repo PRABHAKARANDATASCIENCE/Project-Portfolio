8fc409e84838da70392aa09d901da243eba38290
itcmark4@gmail.com <itcmark4@gmail.com>
2026-09-18 07:01:07 +0000

    ingesting dividend data to bronze table

--- Data Engineer/Project_2/SDP/investment_pipeline_ac844183/transformations/1_bronze/bronze_data_ingestion.sql
+++ Data Engineer/Project_2/SDP/investment_pipeline_ac844183/transformations/1_bronze/bronze_data_ingestion.sql
@@ -11,3 +11,16 @@ FROM STREAM read_files(
   header => true,
   inferSchema => true
 );
+-- dividend ingestion
+CREATE OR REFRESH STREAMING TABLE sdp_investment.bronze.dividend_raw_sdp
+TBLPROPERTIES (
+  'delta.columnMapping.mode' = 'name'
+)
+AS SELECT *,
+  _metadata.file_name AS source_file,
+  _metadata.file_modification_time AS file_mod_time
+FROM STREAM read_files(
+  "/Volumes/sdp_investment/bronze/raw_dividend/",
+  format => "csv",
+  inferSchema => true
+);
