41a8dd311748960997a492209b7121dda1dde59b
itcmark4@gmail.com <itcmark4@gmail.com>
2026-09-18 12:11:01 +0000

    total dividend by stock to gold table

--- Data Engineer/Project_2/SDP/investment_pipeline_ac844183/transformations/3_Gold/gold_bussiness_data.sql
+++ Data Engineer/Project_2/SDP/investment_pipeline_ac844183/transformations/3_Gold/gold_bussiness_data.sql
@@ -8,3 +8,17 @@ coalesce(round((sum(p_and_l)/nullif(sum(invested),0))*100,2),0) as total_unreali
 from sdp_investment.silver.holding_silver_sdp
 group by asset_class,instrument,qty
 order by total_unrealized_pnl desc;
+-- dividend by stock
+create or refresh materialized view sdp_investment.gold.gold_dividend_by_stock
+as 
+with dividend_by_stock as (
+    select instrument,sum(total_dividend) as total_dividend_income,count(*) as dividend_payment
+    from sdp_investment.silver.dividend_silver_sdp
+    group by instrument
+)
+select h.instrument,h.qty,h.invested,h.cur_val,h.p_and_l,
+coalesce(d.total_dividend_income,0) as total_dividend_income,
+coalesce(d.dividend_payment,0) as dividend_payment
+from sdp_investment.silver.holding_silver_sdp h
+left join dividend_by_stock d
+on h.instrument=d.instrument;
